28448260078ed0bc19ff227ea3c19c3b252a7553
Chaturth H. S Gowda <chaturthgowda7777@gmail.com>
2026-02-11 09:36:00 +0530

    Leet Code problem
    Solved problems

---
+++ leet1.java
@@ -0,0 +1,28 @@
+class Add{
+	static String addTwo(String word1, String word2)
+	{
+		String result="";
+		int i=0;
+		while(i<word1.length()||i<word2.length())
+		{
+		if(i<word1.length()){
+				result=result+word1.charAt(i);
+		}
+		if(i<word2.length()){
+			result=result+word2.charAt(i);
+		}
+		i++;
+		}
+		return result;
+		
+	}
+
+public static void main(String[] args)
+{
+	String output=Add.addTwo("abc","pqr");
+	System.out.println(output);
+}
+}
+
+		
+			
