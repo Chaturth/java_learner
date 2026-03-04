70d2d4662a72d91105d46c171f5eb2db6110658f
Chaturth H. S Gowda <chaturthgowda7777@gmail.com>
2026-03-04 11:00:18 +0530

    java

---
+++ Fact.java
@@ -0,0 +1,33 @@
+class Fact {
+    static int fact(int n)
+    {
+        if(n == 1)          
+            return 1;
+
+        return n * fact(n - 1);  
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(fact(5));
+    }
+}
+
+		
+class Fib{
+	static int fib(int n)
+	{
+		if(n==0)
+			return 0;
+		if(n==1)
+			return 1;
+		
+		return fib(n-1) + fib(n-1);
+	}
+	public static void main(String... args)
+	{
+		System.out.println(fib(5));
+	}
+}
+
+
