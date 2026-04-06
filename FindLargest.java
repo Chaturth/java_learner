453ecb2bca06334508aa33909d8639bad4996bcc
Chaturth H. S Gowda <chaturthgowda7777@gmail.com>
2026-04-06 18:50:14 +0530

    java

---
+++ FindLargest.java
@@ -0,0 +1,18 @@
+class FindLargest{
+	public static void main(String[] args)
+	{
+		int num[] = {1,2,3,4,5,6};
+		
+		int largest=num[0];
+		
+		for(int i=0;i<num.length;i++)
+		{
+			if(num[i]>largest)
+			{
+				largest=num[i];
+						
+			}
+		}
+		System.out.println(largest);
+	}
+}
